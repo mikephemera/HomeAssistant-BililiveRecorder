d428d60911a609c9da874c24131419330fb0dd18
Michael Liang <michaelly0128@gmail.com>
2025-02-01 04:17:22 -0600

    v1.1

--- BililiveRecorder/run.sh
+++ BililiveRecorder/run.sh
@@ -11,16 +11,9 @@ PGID=$(bashio::config 'pgid')
 # 创建持久化目录
 mkdir -p ${STORAGE_PATH}
 
-# 设置环境变量
-export BREC_HTTP_OPEN_ACCESS = "true"
-bashio::log.info "BREC_HTTP_OPEN_ACCESS is set to: ${BREC_HTTP_OPEN_ACCESS}"
-
-# 检查所有环境变量
-bashio::log.info "=== 环境变量列表 ==="
-printenv | grep BREC_
+# 在 config.yaml 中显式声明环境变量
 
 # 运行录播姬（绑定到本地回环地址）
 exec /entrypoint.sh \
-    --http-open-access "${BREC_HTTP_OPEN_ACCESS}" \
     --http-ip 127.0.0.1 \
     -d "/rec"
